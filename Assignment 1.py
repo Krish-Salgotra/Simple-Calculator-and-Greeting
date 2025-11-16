2c177cfcaf50b60c453cd659154546d6b46928aa
Krish-Salgotra <chess992006@gmail.com>
2025-11-16 20:55:47 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,15 @@
+#Task 1
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+print("Addition: ", num1 + num2)
+print("Subtraction: ", num1 - num2)
+print("Multiplication: ", num1 * num2)
+print("Division: ", num1 / num2)
+
+#Task 2
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+
+print(f"Hello, {a +" " + b}! Welcome to the Python program.")
